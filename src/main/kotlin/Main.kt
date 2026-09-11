8d611e988d59a1f236b07059ea44779386d6edd2
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-11 15:06:11 +0400

    feat: null handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,8 @@
 fun main() {
-    val number = 23
+    var text: String? = "Name"
 
-    val intergerMaxValue = Int.MAX_VALUE
-    val intergerMinValue = Int.MIN_VALUE
-    println("Max and min values of integers is: $intergerMaxValue and $intergerMinValue respectively")
-
-    val byteMaxValue = Byte.MAX_VALUE
-    val byteMinValue = Byte.MIN_VALUE
-    println("Max and min values of byte is: $byteMaxValue and $byteMinValue respectively")
-
-    val shortMaxValue = Short.MAX_VALUE
-    val shortMinValue = Short.MIN_VALUE
-    println("Max and min values of short is: $shortMaxValue and $shortMinValue respectively")
-
-    val longMaxValue = Long.MAX_VALUE
-    val longMinValue = Long.MIN_VALUE
-    println("Max and min values of long is: $longMaxValue and $longMinValue respectively")
-
-    val myFloatNumber = 2.5F
-    val myDoubleNumber = 2.7
-
-    println("myFloatNumber = $myFloatNumber and myDoubleNumber = $myDoubleNumber")
-
-    val myCharValue = 'a'
-    println(myCharValue)
-
-    val myBooleanValue = true
-    println(myBooleanValue)
+    println(text?.length)
+    text = null
+    val text2 = text ?: "The variable is null"
+    println(text2)
 }
