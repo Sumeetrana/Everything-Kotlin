f5a75f7a663ed5b7acc5de6615939c9852191dcf
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-11 14:02:28 +0400

    feat: numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
 fun main() {
-    println("Hello World!")
+    val number = 23
+
+    val intergerMaxValue = Int.MAX_VALUE
+    val intergerMinValue = Int.MIN_VALUE
+    println("Max and min values of integers is: $intergerMaxValue and $intergerMinValue respectively")
+
+    val byteMaxValue = Byte.MAX_VALUE
+    val byteMinValue = Byte.MIN_VALUE
+    println("Max and min values of byte is: $byteMaxValue and $byteMinValue respectively")
+
+    val shortMaxValue = Short.MAX_VALUE
+    val shortMinValue = Short.MIN_VALUE
+    println("Max and min values of short is: $shortMaxValue and $shortMinValue respectively")
+
+    val longMaxValue = Long.MAX_VALUE
+    val longMinValue = Long.MIN_VALUE
+    println("Max and min values of long is: $longMaxValue and $longMinValue respectively")
+
+    val myFloatNumber = 2.5F
+    val myDoubleNumber = 2.7
+
+    println("myFloatNumber = $myFloatNumber and myDoubleNumber = $myDoubleNumber")
 }
