58d93d5ed078e8632ab903d0f5635c6f2a08a98a
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-14 13:00:57 +0400

    feat: interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,27 @@
 import javax.xml.crypto.Data
 
 fun main() {
-    val user1 = User("Alex", "Dobbin", 23)
-    val user2 = User("Alex", "Dobbin", 23)
 
-    println(user1 == user2)
-    println(user1)
 }
 
-//class User(var firstName: String, var lastName: String, var age: Int) {
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) {
-//            return true
-//        }
-//
-//        if(other is User) {
-//            return this.firstName == other.firstName
-//                    && this.lastName == other.lastName
-//                    && this.age == other.age
-//        }
-//
-//        return false
-//    }
-//
-//    override fun hashCode(): Int {
-//        return 0
-//    }
-//}
+interface Engine {
+    fun startEngine()
+}
 
-// Data class generates all of the code that we wrote above and many other useful code as well
-data class User(var firstName: String, var lastName: String, var age: Int)
+class Car(val name: String, val color: String): Engine {
+    override fun startEngine() {
+        println("The car is starting the engine")
+    }
+}
+
+class Truck(val name: String, val color: String): Engine {
+    override fun startEngine() {
+        println("The truck is starting the engine")
+    }
+}
+
+class Plane(val name: String, val color: String): Engine {
+    override fun startEngine() {
+        println("The plane is starting the engine")
+    }
+}
