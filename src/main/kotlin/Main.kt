626e01d1cc57397060bc3eeb07ef1e7fd68ef61c
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-14 10:16:03 +0400

    feat: lateinit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,10 @@
 fun main() {
-    val user1 = User("Alex")
-    val user2 = User("Smith", "Dobinca")
-    val user3 = User()
+    val user = User("Alex", "Dobbin", 23)
 
-    println("Name = ${user1.firstName}")
-    println("LastName = ${user1.lastName}")
-    println("Age = ${user1.age}")
-
-    println("\n")
-
-    println("Name = ${user2.firstName}")
-    println("LastName = ${user2.lastName}")
-    println("Age = ${user2.age}")
-
-    println("\n")
-
-    println("Name = ${user3.firstName}")
-    println("LastName = ${user3.lastName}")
-    println("Age = ${user3.age}")
+    user.favoriteMovie = "Interstellar"
+    println(user.favoriteMovie)
 }
 
 class User(firstName: String = "FirstName", var lastName: String = "LastName", var age: Int = 0) {
-    var firstName = firstName
-        get() {
-            return "Called getter $field"
-        }
-
-        set(value) {
-            println("Called setter $field")
-            field = value
-        }
-  }
+    lateinit var favoriteMovie: String
 }
