ff89d6247027f968d7960eabcaebf788ce459842
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-16 16:03:30 +0400

    feat: lambda functions

--- src/main/kotlin/com/example/main/Main.kt
+++ src/main/kotlin/com/example/main/Main.kt
@@ -1,13 +1,40 @@
 package com.example.main
 
-fun main() {
-    val numbers = arrayOf(1,2,3,4)
 
-    try {
-        println(numbers[5])
-    } catch (e: ArrayIndexOutOfBoundsException) {
-        println("Out of range of bounds: ${e.message}")
-    } finally {
-        println("Access the element")
-    }
+fun main() {
+    val myLambda = {a: Int, b: Int -> println(a+b)}
+    myLambda(5,10)
+
+    add(5, 10, {a: Int -> println(a+a)})
+
+
+    val loginButton = Button("Login", 34345, object : OnClickListener {
+        override fun onClick() {
+
+        }
+    })
+
+    val signupButton = Button("Signup", 2345, object : OnClickListener {
+        override fun onClick() {
+
+        }
+    })
+
+    upperCase("hello", {s: String -> s.uppercase()})
+    upperCase("hello"){it.uppercase()}
+}
+
+fun add(a: Int, b: Int, action: (Int) -> Unit) {
+    action(a+b)
+}
+
+class Button(val text: String, val id: Int, val onClickListener: OnClickListener)
+
+interface OnClickListener {
+    fun onClick()
+}
+
+fun upperCase(str: String, myFunction: (String) -> String) {
+    val uppercasedWord = myFunction(str)
+    println(uppercasedWord)
 }
