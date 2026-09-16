b890072e2080509d910149d7acc117c02ae2fcb1
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-16 11:30:41 +0400

    feat: binary search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,7 @@
 import javax.xml.crypto.Data
 
 fun main() {
-    val numbers = mutableListOf(2,5,1,40,20,100,60)
-    numbers.sorted().forEach { println(it) }
-
-    val laptops = mutableListOf(
-        Laptop("Dell", 2020, 2, 600),
-        Laptop("Apple", 2024, 1, 1000),
-        Laptop("Acer", 2022, 4, 800),
-    )
-    println()
-    laptops.sorted().forEach { println(it)}
-    println()
-    laptops.sortedWith(ComparatorRam()).forEach { println(it) }
-
-    println()
-    laptops.sortedWith(compareBy {it.year}).forEach { println(it) }
-
-    println()
-    laptops.sortedBy{it.ram}.forEach{println(it)}
-
-    println()
-    laptops.sortedWith(compareBy<Laptop>{it.year}.thenBy{it.price}).forEach{println(it)}
+    println(searchElement(27, mutableListOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14, 15,16,17,18,19, 20,21,22,23,24,25, 26, 27,28,29,30)))
 }
 
-data class Laptop(val brand: String, val year: Int, val ram: Int, val price: Int): Comparable<Laptop> {
-    override fun compareTo(other: Laptop): Int {
-        if(this.price > other.price) return 1
-        else if (this.price < other.price) return -1
-        return 0
-    }
-}
-
-class ComparatorRam: Comparator<Laptop> {
-    override fun compare(o1: Laptop, o2: Laptop): Int {
-        return if (o1.ram > o2.ram) {
-            1
-        } else if(o1.ram < o2.ram) {
-            -1
-        } else{
-            0
-        }
-    }
-}
+private fun searchElement(searchedElement: Int, numbers: MutableList<Int>): Int = numbers[numbers.binarySearch(27)]
