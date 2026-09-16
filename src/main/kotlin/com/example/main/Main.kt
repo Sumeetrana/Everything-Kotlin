60280bb6fb958f1b3451f5522fc523dae50a230c
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-16 17:06:57 +0400

    feat: coroutines basic

--- src/main/kotlin/com/example/main/Main.kt
+++ src/main/kotlin/com/example/main/Main.kt
@@ -1,21 +1,26 @@
 package com.example.main
 
-import kotlin.concurrent.thread
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    println("Hello world 1")
-    println("Hello world 2")
-    println("Hello world 3")
-    println("Hello world 4")
-    println("Hello world 5")
+    println("Main program starts: ${Thread.currentThread().name}")
 
-    thread {
-        Thread.sleep(5000)
-        println("Thread 2 has finished.")
+    val parentJob = CoroutineScope(Default).launch {
+        println("Fake work starts: ${Thread.currentThread().name}")
+        delay(2000)
+        println("Fake work finished: ${Thread.currentThread().name}")
     }
 
-    println("Hello world 6")
-    println("Hello world 7")
-    println("Hello world 8")
-    println("Hello world 9")
+    runBlocking {
+        parentJob.join()
+    }
+
+
+    println("Main program ends: ${Thread.currentThread().name}")
+
+
 }
