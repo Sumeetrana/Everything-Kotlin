fa72eda6dd349e2a2158b21d822c96aec30e8825
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-16 11:22:41 +0400

    feat: comparable, comparator and sorting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,45 @@
 import javax.xml.crypto.Data
 
 fun main() {
-    val numbers = listOf(6, 10, 14, 4, 100)
-    println("The sum is ${numbers.sum()}")
-    println("The count is: ${numbers.count()}")
-    println("The average is: ${numbers.average()}")
-    println("The max value is: ${numbers.maxOrNull()}")
-    println("The min value is: ${numbers.minOrNull()}")
-    println("The sum is: ${numbers.sumOf{ it * 2}}")
+    val numbers = mutableListOf(2,5,1,40,20,100,60)
+    numbers.sorted().forEach { println(it) }
 
+    val laptops = mutableListOf(
+        Laptop("Dell", 2020, 2, 600),
+        Laptop("Apple", 2024, 1, 1000),
+        Laptop("Acer", 2022, 4, 800),
+    )
+    println()
+    laptops.sorted().forEach { println(it)}
+    println()
+    laptops.sortedWith(ComparatorRam()).forEach { println(it) }
+
+    println()
+    laptops.sortedWith(compareBy {it.year}).forEach { println(it) }
+
+    println()
+    laptops.sortedBy{it.ram}.forEach{println(it)}
+
+    println()
+    laptops.sortedWith(compareBy<Laptop>{it.year}.thenBy{it.price}).forEach{println(it)}
+}
+
+data class Laptop(val brand: String, val year: Int, val ram: Int, val price: Int): Comparable<Laptop> {
+    override fun compareTo(other: Laptop): Int {
+        if(this.price > other.price) return 1
+        else if (this.price < other.price) return -1
+        return 0
+    }
+}
+
+class ComparatorRam: Comparator<Laptop> {
+    override fun compare(o1: Laptop, o2: Laptop): Int {
+        return if (o1.ram > o2.ram) {
+            1
+        } else if(o1.ram < o2.ram) {
+            -1
+        } else{
+            0
+        }
+    }
 }
