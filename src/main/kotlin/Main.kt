6959fa7427e6dca07bd145a5de3e04ffc5f27953
Sumeet rana <sumeetsinh28@gmail.com>
2026-09-16 11:57:39 +0400

    feat: counter variance in generics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
 import javax.xml.crypto.Data
 
 fun main() {
-    val footballTeam = Team<Player>(
-        name = "Barcelona",
-        mutableListOf<FootballPlayer>(FootballPlayer("messi"), FootballPlayer("ronaldo"))
+//    val footballTeam = Team<Player>(
+//        name = "Barcelona",
+//        mutableListOf<FootballPlayer>(FootballPlayer("messi"), FootballPlayer("ronaldo"))
+//    )
+
+    val gamesTeam = Team<CounterStrikePlayer>(
+        "Games Team",
+        mutableListOf<GamesPlayer>(GamesPlayer("Player 1"), GamesPlayer("Player 2"))
     )
 }
 
-class Team<T: Player>(val name: String, val players: MutableList<out T>) {
+class Team<T: Player>(val name: String, val players: MutableList<in T>) {
     fun addPlayers(player: T) {
         if (players.contains(player)) {
             println("Player ${player.name} is already playing!")
